17289a262d94dc6e936a682dff275aefba3c77d3
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-24 13:57:58 -0700

    Update boilerplate files to begin project
    Co-authored-by: Marney Mallory <marney.mallory@gmail.com>

---
+++ University/Models/Student.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace University.Models
+{
+  public class Student
+  {
+    public Student()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+
+    }
+
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public DateTime Enrollment { get; set; }
+    public virtual ICollection<CourseStudent> JoinEntities { get; }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
